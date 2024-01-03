c2e689fc89ac4f628dd1a8187056e2712f9a2777
adetunji <adekunleadetunjiwilson@gmail.com>
2024-01-03 14:58:43 +0100

    corrected Eslint error

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -1,6 +1,7 @@
 export default function appendToEachArrayValue(array, appendString) {
   let idx = 0;
   for (const val of array) {
+    /* eslint-disable no-param-reassign */
     array[idx] = appendString + val;
     idx += 1;
   }
