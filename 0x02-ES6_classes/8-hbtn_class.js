445ea1d0080524020afbe6113d601d5f54bf9648
adetunji <adekunleadetunjiwilson@gmail.com>
2024-01-05 18:53:55 +0100

    fix task 8

--- 0x02-ES6_classes/8-hbtn_class.js
+++ 0x02-ES6_classes/8-hbtn_class.js
@@ -1,28 +1,12 @@
 export default class HolbertonClass {
-  constructor(size, location) {
-    this.size = size;
-    this.location = location;
+    constructor(size, location) {
+      this._size = size;
+      this._location = location;
+    }
+  
+    [Symbol.toPrimitive](hint) {
+      if (hint === 'number') return this._size;
+      if (hint === 'string') return this._location;
+      return this;
+    }
   }
-
-  set size(size) {
-    this._size = size;
-  }
-
-  get size() {
-    return this._size;
-  }
-
-  set location(location) {
-    this._location = location;
-  }
-
-  get location() {
-    return this._location;
-  }
-
-  [Symbol.toPrimitive](hint) {
-    if (typeof hint === 'number') return this.size;
-    if (typeof hint === 'string') return this.location;
-    return this;
-  }
-}
