db4ec821e7f442a376d8118c30d5f1a69b11b824
adetunji <adekunleadetunjiwilson@gmail.com>
2024-01-09 15:43:45 +0100

    fix for task 8 checker error

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,10 +1,11 @@
 export default function cleanSet(set, startstring) {
   const arr = Array.from(set);
-  if (startstring.length < 1) return '';
-  const processed = arr.filter((elem) => elem.startsWith(startstring)).map((elem) => {
-    const len = startstring.length;
-    return elem.slice(len);
-  });
+  if ((startstring.length < 1) || typeof startstring !== 'string') return '';
+  const processed = arr.filter((elem) => (elem.startsWith(startstring) && (typeof elem === 'string')))
+    .map((elem) => {
+      const len = startstring.length;
+      return elem.slice(len);
+    });
 
   return processed.join('-');
 }
