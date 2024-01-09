569b271ecbab21784402e2268c1d00726a852a01
adetunji <adekunleadetunjiwilson@gmail.com>
2024-01-09 15:54:04 +0100

    new fix for task 8

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,7 +1,7 @@
 export default function cleanSet(set, startstring) {
   const arr = Array.from(set);
-  if ((startstring.length < 1) || typeof startstring !== 'string') return '';
-  const processed = arr.filter((elem) => (elem.startsWith(startstring) && (typeof elem === 'string')))
+  if ((startstring === '') || typeof startstring !== 'string') return '';
+  const processed = arr.filter((elem) => ((typeof elem === 'string') && elem.startsWith(startstring)))
     .map((elem) => {
       const len = startstring.length;
       return elem.slice(len);
